365160103335d8a3231ea64d8dffeb5dfe01900b
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-09 13:29:57 +0200

    Create manager_colors to control all colors of application

---
+++ lib/core/resource/manager_colors.dart
@@ -0,0 +1,29 @@
+import '/config/all_imports.dart';
+
+class ManagerColors {
+  static const Color primaryColor = Color(0XFF464698);
+
+  ///s1
+  static const Color spaceCadet = Color(0XFF292959);
+
+  ///s2
+  static const Color coolGrey = Color(0XFF9090C1);
+
+  ///s4
+  static const Color antiFlashWhite = Color(0XFFF1F1F9);
+
+  ///s5
+  static const Color lavenderSoft = Color(0XFFE0E0FC);
+
+  ///s6
+  static const Color lavenderBright = Color(0XFFE8E8FD);
+
+  ///s7
+  static const Color ghostWhite = Color(0XFFF7F7FE);
+
+  static const Color errorColor = Color(0XFFD51818);
+  static const Color successColor = Colors.green;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+}
