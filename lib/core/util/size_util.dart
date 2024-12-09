5e44d137333479533605382e960f3eaffdc83b76
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-09 13:35:08 +0200

    Create size_util to make sizes responsive on all phone

---
+++ lib/core/util/size_util.dart
@@ -0,0 +1,23 @@
+import '/config/all_imports.dart';
+
+class SizeUtil {
+  static double setWidth(double width) {
+    return width.w;
+  }
+
+  static double setHeight(double height) {
+    return height.h;
+  }
+
+  static double setRadius(double radius) {
+    return radius.r;
+  }
+
+  static double setFontSize(double fontSize) {
+    return fontSize.sp;
+  }
+
+  static double setIconSize(double iconSize) {
+    return iconSize.r;
+  }
+}
