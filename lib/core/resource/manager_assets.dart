b92030d9c37af7d51d7703eeb7f682974a3b3596
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-09 13:29:42 +0200

    Create manager_assets to control all assets of application

---
+++ lib/core/resource/manager_assets.dart
@@ -0,0 +1,39 @@
+const String _imagePath = 'assets/images';
+
+class ManagerAssets {
+  static const String logo = '$_imagePath/add_product.svg';
+
+  //departments image
+  static const String bicycle = '$_imagePath/departments/bicycle.svg';
+  static const String car = '$_imagePath/departments/car.svg';
+  static const String carpet = '$_imagePath/departments/carpet.svg';
+  static const String computer = '$_imagePath/departments/computer.svg';
+  static const String electricMachines =
+      '$_imagePath/departments/electric_machines.svg';
+  static const String furniture = '$_imagePath/departments/furniture.svg';
+  static const String motorcycle = '$_imagePath/departments/motorcycle.svg';
+  static const String phone = '$_imagePath/departments/phone.svg';
+
+  //login_and_create_account image
+  static const String successfullyState =
+      '$_imagePath/login_and_create_account/successfully_state.svg';
+  static const String addInformation =
+      '$_imagePath/login_and_create_account/add_information.svg';
+  static const String createAccount =
+      '$_imagePath/login_and_create_account/create_account.svg';
+  static const String partyDecoration =
+      '$_imagePath/login_and_create_account/party_decoration.svg';
+  static const String happyPerson =
+      '$_imagePath/login_and_create_account/happy_person.svg';
+  static const String login = '$_imagePath/login_and_create_account/login.svg';
+  static const String securityCode =
+      '$_imagePath/login_and_create_account/security_code.svg';
+
+  //on_boarding image
+  static const String onBoardingOne =
+      '$_imagePath/on_boarding/on_boarding_one.svg';
+  static const String onBoardingTwo =
+      '$_imagePath/on_boarding/on_boarding_two.svg';
+  static const String onBoardingTitle =
+      '$_imagePath/on_boarding/on_boarding_title .svg';
+}
