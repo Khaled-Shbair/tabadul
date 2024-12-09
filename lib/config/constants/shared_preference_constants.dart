11b4f68a821ef147414d52978216397934cdbbce
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-09 13:39:54 +0200

    Create shared preference constants contain keys (constants) of shared preference

---
+++ lib/config/constants/shared_preference_constants.dart
@@ -0,0 +1,12 @@
+class SharedPreferenceConstants {
+  static const String loggedIn = 'logged_in';
+  static const String phone = 'phone';
+  static const String id = 'id';
+  static const String viewOnBoarding = 'view_on_boarding';
+  static const String verificationId = 'verification_id';
+  static const String firstName = 'first_name';
+  static const String lastName = 'last_name';
+  static const String city = 'city';
+  static const String image = 'image';
+  static const String street = 'street';
+}
