6f21b3a13d13bd921dcd0871494b81b22c3309cb
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-09 13:32:32 +0200

    Create manager_strings to control all strings in application

---
+++ lib/core/resource/manager_strings.dart
@@ -0,0 +1,6 @@
+import '/config/all_imports.dart';
+
+class ManagerStrings {
+  static String noRouteFound = 'noRouteFound'.tr;
+  static String pleaseEnterCodeCorrectly = 'please_enter_code_correctly'.tr;
+}
