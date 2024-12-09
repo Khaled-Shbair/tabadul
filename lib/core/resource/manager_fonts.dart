0c519ea99e5f06d0705aca9c402f6cc07ad8e99b
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-09 13:31:39 +0200

    Create manager_fonts to control all style of fonts in application

---
+++ lib/core/resource/manager_fonts.dart
@@ -0,0 +1,29 @@
+import '/config/all_imports.dart';
+
+class ManagerFontsSizes {
+  static double f8 = SizeUtil.setFontSize(8);
+  static double f10 = SizeUtil.setFontSize(10);
+  static double f12 = SizeUtil.setFontSize(12);
+  static double f14 = SizeUtil.setFontSize(14);
+  static double f16 = SizeUtil.setFontSize(16);
+  static double f18 = SizeUtil.setFontSize(18);
+  static double f20 = SizeUtil.setFontSize(20);
+  static double f22 = SizeUtil.setFontSize(22);
+  static double f24 = SizeUtil.setFontSize(24);
+  static double f26 = SizeUtil.setFontSize(26);
+  static double f28 = SizeUtil.setFontSize(28);
+  static double f30 = SizeUtil.setFontSize(30);
+}
+
+class ManagerFontWeight {
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight extraBold = FontWeight.w800;
+}
+
+class ManagerFontFamily {
+  static const String kHelveticaL = 'HelveticaLTArabic';//bold
+  static const String kHelveticaLNeeL = 'HelveticaNeueLTRoman';
+}
