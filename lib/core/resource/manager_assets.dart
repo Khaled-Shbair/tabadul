4698285de75a2bf988c6e230e637c91b8c3c8b91
Khaled Shbair <khaled.shbair12@gmail.com>
2025-04-27 11:30:07 +0300

    add new images assets

--- lib/core/resource/manager_assets.dart
+++ lib/core/resource/manager_assets.dart
@@ -1,7 +1,20 @@
 const String _imagePath = 'assets/images';
 
 class ManagerAssets {
-  static const String logo = '$_imagePath/add_product.svg';
+  static const String logo = '$_imagePath/logo.svg';
+
+  //splash
+  static const String splashLogo = '$_imagePath/splash/splash_logo.svg';
+
+  static const String addProduct = '$_imagePath/add_product.svg';
+
+  //icons image
+  static const String homeIcon = '$_imagePath/icons_images/home_icon.svg';
+  static const String notificationIcon =
+      '$_imagePath/icons_images/notification_icon.svg';
+  static const String userIcon = '$_imagePath/icons_images/user_icon.svg';
+  static const String registerAsServiceProviderIcon =
+      '$_imagePath/icons_images/register_as_service_provider_icon.svg';
 
   //departments image
   static const String bicycle = '$_imagePath/departments/bicycle.svg';
@@ -30,10 +43,15 @@ class ManagerAssets {
       '$_imagePath/login_and_create_account/security_code.svg';
 
   //on_boarding image
-  static const String onBoardingOne =
-      '$_imagePath/on_boarding/on_boarding_one.svg';
-  static const String onBoardingTwo =
-      '$_imagePath/on_boarding/on_boarding_two.svg';
-  static const String onBoardingTitle =
-      '$_imagePath/on_boarding/on_boarding_title .svg';
+  static const String deliveryDriverAvatar =
+      '$_imagePath/on_boarding/delivery_driver_avatar.svg';
+  static const String backgroundDeliveryDriverAvatar =
+      '$_imagePath/on_boarding/background_delivery_driver_avatar.svg';
+  static const String woman = '$_imagePath/on_boarding/woman_avatar.svg';
+  static const String man = '$_imagePath/on_boarding/man_avatar.svg';
+  static const String leftPhone = '$_imagePath/on_boarding/left_phone.svg';
+  static const String rightPhone = '$_imagePath/on_boarding/right_phone.svg';
+  static const String arrows = '$_imagePath/on_boarding/arrows.svg';
+  static const String flowers = '$_imagePath/on_boarding/flowers.svg';
+  static const String addedSuccessfully = '$_imagePath/checking_boxes.svg';
 }
