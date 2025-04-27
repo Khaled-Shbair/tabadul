2e744a0dce0d926bb67317d9aad97952a4e73979
Khaled Shbair <khaled.shbair12@gmail.com>
2025-04-27 11:28:48 +0300

    add new string

--- lib/core/resource/manager_strings.dart
+++ lib/core/resource/manager_strings.dart
@@ -1,6 +1,29 @@
 import '/config/all_imports.dart';
 
 class ManagerStrings {
-  static String noRouteFound = 'noRouteFound'.tr;
+  static String noRouteFound = 'no_route_found'.tr;
+  static String noInternetConnection = 'no_internet_connection'.tr;
+  static String invalidVerificationCode = 'invalid_verification_code'.tr;
+  static String userNotRegistered = 'user_not_registered'.tr;
+  static String badRequest = 'bad_request'.tr;
+  static String oopsThereIsSomethingWrong = 'oops_there_is_something_wrong'.tr;
   static String pleaseEnterCodeCorrectly = 'please_enter_code_correctly'.tr;
+  static String logout = 'logout'.tr;
+  static String login = 'login'.tr;
+  static String loginButton = 'login_button'.tr;
+  static String cancel = 'cancel'.tr;
+  static String enterPhoneNumber = 'enter_phone_number'.tr;
+  static String doNotHaveAnAccount = 'do_not_have_an_account'.tr;
+  static String createAccount = 'create_account'.tr;
+  static String pleaseEnterTheNumberCorrectly =
+      'please_enter_the_number_correctly'.tr;
+  static String doYouWantLogoutOfYourAccount =
+      'do_you_want_logout_of_your_account'.tr;
+  static String fairReturnPolicy = 'fair_return_policy'.tr;
+  static String subTitleOnBoardingOne = 'sub_title_on_boarding_one'.tr;
+  static String subTitleOnBoardingTwo = 'sub_title_on_boarding_two'.tr;
+  static String fastDelivery = 'fast_delivery'.tr;
+  static String securityCode = 'security_code'.tr;
+  static String securityCodeSubtitle = 'security_code_subtitle'.tr;
+  static String resendCode = 'resend_code'.tr;
 }
