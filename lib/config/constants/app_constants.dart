04495e5009a59a08263f82ab6bd6999bf7dafef5
Khaled Shbair <khaled.shbair12@gmail.com>
2025-04-27 11:15:25 +0300

    Create app_constants file contain all constants of application

---
+++ lib/config/constants/app_constants.dart
@@ -0,0 +1,15 @@
+import '../all_imports.dart';
+
+class AppConstants {
+  static const double heightDevice = 760;
+  static const double widthDevice = 360;
+  static const int timeoutVerifyPhoneNumber = 60;
+  static const double elevationAppBarTheme = 0;
+  static const double widthOfSideOfBorderSideInMainButton = 1;
+  static const double elevationOfMyBackButton = 5;
+  static const int maxLengthOfPhoneNumber = 10;
+  static const List<CodeCountryModel> countriesCodes = [
+    CodeCountryModel(name: '970+', id: 0),
+    CodeCountryModel(name: '972+', id: 1),
+  ];
+}
