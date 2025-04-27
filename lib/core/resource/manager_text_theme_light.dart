31be0f7773b815fabdff681422dcb52ccfffc131
Khaled Shbair <khaled.shbair12@gmail.com>
2025-04-27 11:29:26 +0300

    add new text style theme in light mode

--- lib/core/resource/manager_text_theme_light.dart
+++ lib/core/resource/manager_text_theme_light.dart
@@ -2,5 +2,37 @@ import '/config/all_imports.dart';
 
 class ManagerTextThemeLight extends TextTheme {
   @override
-  TextStyle get displayBold => TextStyle();
+  TextStyle get titleLarge => TextStyle();
+
+  @override
+  TextStyle get titleMedium => ManagerTextStyles.font20PrimaryColorBold;
+
+  @override
+  TextStyle get titleSmall => ManagerTextStyles.font15WhiteBold;
+
+  // sub title on boarding screen
+  @override
+  TextStyle get headlineSmall => ManagerTextStyles.font15SpaceCadetBold;
+
+  @override
+  TextStyle get labelLarge => ManagerTextStyles.font12WhiteBold;
+
+  /// [phone number of country in login screen]
+  @override
+  TextStyle get displaySmall => ManagerTextStyles.font14PrimaryColorMedium;
+
+  /// richText button in login and create account
+  @override
+  TextStyle get displayMedium => ManagerTextStyles.font14SpaceCadetBold;
+
+  /// style of text error
+  @override
+  TextStyle get bodySmall => ManagerTextStyles.font12ErrorColorBold;
+
+  /// [logout button]
+  @override
+  TextStyle get labelMedium => ManagerTextStyles.font9WhiteBold;
+
+  @override
+  TextStyle get labelSmall => ManagerTextStyles.font9PrimaryColorBold;
 }
