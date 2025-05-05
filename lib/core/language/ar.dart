8fcd70f612606ceb2d0bb4b20c080f8209801ea8
Khaled Shbair <khaled.shbair12@gmail.com>
2025-05-05 13:47:24 +0300

    edit on words

--- lib/core/language/ar.dart
+++ lib/core/language/ar.dart
@@ -1,18 +1,5 @@
 const Map<String, String> ar = {
-  'logout': 'تسجيل خروج',
-  'no_internet_connection': 'لا يوجد اتصال بالإنترنت',
-  'user_not_registered': 'المستخدم غير مسجل',
-  'oops_there_is_something_wrong': 'عذراً، هناك شي ما خطا',
-  'do_you_want_logout_of_your_account': 'هل تريد تسجيل الخروج\nمن الحساب ؟',
-  'cancel': 'إلغاء',
-  'enter_phone_number': 'أدخل رقمك الهاتفي',
-  'login': 'تسجيل الدخول',
-  'login_button': 'تسجيل دخول',
-  'please_enter_the_number_correctly': 'يرجى إدخال الرقم بشكل صحيح',
-  'create_account': 'إنشاء حساب',
-  'do_not_have_an_account': 'ليس لديك حساب من قبل ؟ ',
-  'already_have_an_account': 'هل لديك حساب من قبل ؟ ',
-  'please_enter_code_correctly': 'يرجى إدخال الرمز بشكل صحيح',
+  //on boarding strings
   'fair_return_policy': 'سياسة إرجاع عادلة',
   'sub_title_on_boarding_one':
       'نضمن إرجاع السلع دون دفع أي رسوم ويمكنك إرجاع أي سلعة لم تعد ترغب بها مع دفع تكلفة التوصيل فقط',
@@ -20,27 +7,55 @@ const Map<String, String> ar = {
   'sub_title_on_boarding_two':
       'نتبع سياسة توصيل آمنة تضمن وصول السلعة لباب منزلك خلال 24 ساعة أو أقل',
 
-  'resend_code': 'إعادة إرسال الرمز',
-  'security_code_subtitle':
-  ' لقد أرسلنا لك رسالة نصية قصيرة\n تحتوي على الرمز سيتم تفعيلها تلقائيا',
+  //auth string
+  'login': 'تسجيل الدخول',
+  'create_account': 'إنشاء حساب',
+  'enter_phone_number': 'أدخل رقمك الهاتفي',
+  'do_not_have_an_account': 'ليس لديك حساب من قبل ؟ ',
+  'already_have_an_account': 'هل لديك حساب من قبل ؟ ',
+  'please_enter_the_number_correctly': 'يرجى إدخال الرقم بشكل صحيح',
+  'the_number_is_not_registered': 'الرقم غير مسجل',
   'security_code': 'رمز الأمان',
-  ///////////////////
+  'resend_code': 'إعادة إرسال الرمز',
+  'please_enter_code_correctly': 'يرجى إدخال الرمز بشكل صحيح',
+  'invalid_verification_code': 'رمز التحقق الذي أدخلته غير صحيح',
+  'security_code_subtitle':
+      ' لقد أرسلنا لك رسالة نصية قصيرة\n تحتوي على الرمز سيتم تفعيلها تلقائيا',
 
+  'the_account_has_been_created_successfully': 'لقد تم إنشاء الحساب\n بنجاح',
+  'the_number_is_already_registered': 'الرقم مسجل بالفعل',
+
+  //add personal information strings
+  'add_information': 'إضافة معلومات',
+  'first_name': 'الاسم الأول',
+  'last_name': 'الاسم الأخير',
+  'city': 'المدينة',
+  'region_street': 'المنطقة / الشارع',
+  'please_select_city': 'يرجى اختيار المدينة',
+  'please_enter_first_name': 'يرجى ادخال الاسم الأول',
+  'please_enter_last_name': 'يرجى ادخال الاسم الأخير',
+  'please_select_region_street': 'يرجى اختيار المنطقة و الشارع',
+  'next': 'التالي',
+
+  //logout string
+  'logout': 'تسجيل خروج',
+  'do_you_want_logout_of_your_account': 'هل تريد تسجيل الخروج\nمن الحساب ؟',
+
+  //errors messages
+  'no_internet_connection': 'لا يوجد اتصال بالإنترنت',
+  'no_data': 'لا يوجد بيانات',
+  'oops_there_is_something_wrong': 'عذراً، هناك شي ما خطا',
+
+  'cancel': 'إلغاء',
+  'search': 'بحث',
+  'no_result_found': 'لا توجد نتائج',
 ////////////////////////////////////////////////
   'the_time_has_been_set_successfully':
       'لقد تم تحديد الوقت بنجاح الرجاء الانتظار حتى انتهاء الوقت للاستلام',
   'notifications': 'الإشعارات',
-  'main': 'الرئيسية',
+  'home': 'الرئيسية',
   'profile': 'الملف الشخصي',
 
-  'add_information': 'إضافة معلومات',
-  'next': 'التالي',
-  'first_name': 'الاسم الأول',
-  'last_name': 'الاسم الأخير',
-  'city': 'المدينة',
-  'area_street': 'المنطقة / الشارع',
-  'the_account_has_been_created_successfully': 'لقد تم إنشاء الحساب\n بنجاح',
-  'the_number_is_already_registered': 'الرقم مسجل بالفعل',
   'furniture_department': 'قسم الأثاث',
   'carpets_and_mattresses_department': 'قسم السجاد والمفارش',
   'phone_department': 'قسم أجهزة الجوال',
--- lib/core/resource/manager_strings.dart
+++ lib/core/resource/manager_strings.dart
@@ -1,29 +1,56 @@
 import '/config/all_imports.dart';
 
 class ManagerStrings {
-  static String noRouteFound = 'no_route_found'.tr;
-  static String noInternetConnection = 'no_internet_connection'.tr;
-  static String invalidVerificationCode = 'invalid_verification_code'.tr;
-  static String userNotRegistered = 'user_not_registered'.tr;
-  static String badRequest = 'bad_request'.tr;
-  static String oopsThereIsSomethingWrong = 'oops_there_is_something_wrong'.tr;
-  static String pleaseEnterCodeCorrectly = 'please_enter_code_correctly'.tr;
-  static String logout = 'logout'.tr;
-  static String login = 'login'.tr;
-  static String loginButton = 'login_button'.tr;
-  static String cancel = 'cancel'.tr;
-  static String enterPhoneNumber = 'enter_phone_number'.tr;
-  static String doNotHaveAnAccount = 'do_not_have_an_account'.tr;
-  static String createAccount = 'create_account'.tr;
-  static String pleaseEnterTheNumberCorrectly =
-      'please_enter_the_number_correctly'.tr;
-  static String doYouWantLogoutOfYourAccount =
-      'do_you_want_logout_of_your_account'.tr;
+  //on boarding strings
   static String fairReturnPolicy = 'fair_return_policy'.tr;
   static String subTitleOnBoardingOne = 'sub_title_on_boarding_one'.tr;
   static String subTitleOnBoardingTwo = 'sub_title_on_boarding_two'.tr;
   static String fastDelivery = 'fast_delivery'.tr;
+
+  //auth string
+  static String login = 'login'.tr;
+  static String noResultFound = 'no_result_found'.tr;
+  static String createAccount = 'create_account'.tr;
+  static String search = 'search'.tr;
+  static String enterPhoneNumber = 'enter_phone_number'.tr;
+  static String doNotHaveAnAccount = 'do_not_have_an_account'.tr;
+  static String alreadyHaveAnAccount = 'already_have_an_account'.tr;
+  static String theNumberIsNotRegistered = 'the_number_is_not_registered'.tr;
   static String securityCode = 'security_code'.tr;
   static String securityCodeSubtitle = 'security_code_subtitle'.tr;
   static String resendCode = 'resend_code'.tr;
+  static String pleaseEnterCodeCorrectly = 'please_enter_code_correctly'.tr;
+  static String theNumberIsAlreadyRegistered =
+      'the_number_is_already_registered'.tr;
+  static String theAccountHasBeenCreatedSuccessfully =
+      'the_account_has_been_created_successfully'.tr;
+  static String pleaseEnterTheNumberCorrectly =
+      'please_enter_the_number_correctly'.tr;
+
+//add personal information strings
+  static String addInformation = 'add_information'.tr;
+  static String firstName = 'first_name'.tr;
+  static String lastName = 'last_name'.tr;
+  static String city = 'city'.tr;
+  static String regionAndStreet = 'region_street'.tr;
+  static String next = 'next'.tr;
+  static String pleaseSelectCity = 'please_select_city'.tr;
+  static String pleaseEnterFirstName = 'please_enter_first_name'.tr;
+  static String pleaseEnterLastName = 'please_enter_last_name'.tr;
+  static String pleaseSelectRegionStreet = 'please_select_region_street'.tr;
+
+  // logout strings
+  static String logout = 'logout'.tr;
+  static String doYouWantLogoutOfYourAccount =
+      'do_you_want_logout_of_your_account'.tr;
+
+  //errors messages
+  static String noRouteFound = 'no_route_found'.tr;
+  static String noInternetConnection = 'no_internet_connection'.tr;
+  static String invalidVerificationCode = 'invalid_verification_code'.tr;
+  static String badRequest = 'bad_request'.tr;
+  static String noData = 'no_data'.tr;
+
+  static String cancel = 'cancel'.tr;
+  static String oopsThereIsSomethingWrong = 'oops_there_is_something_wrong'.tr;
 }
