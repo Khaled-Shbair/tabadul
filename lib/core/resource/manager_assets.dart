f168ceb343a28c8d8366e9b84fb0ed88e6ca32da
Khaled Shbair <khaled.shbair12@gmail.com>
2025-05-05 13:51:48 +0300

    edit path of images

--- lib/core/resource/manager_assets.dart
+++ lib/core/resource/manager_assets.dart
@@ -3,9 +3,36 @@ const String _imagePath = 'assets/images';
 class ManagerAssets {
   static const String logo = '$_imagePath/logo.svg';
 
-  //splash
+  //splash images
   static const String splashLogo = '$_imagePath/splash/splash_logo.svg';
 
+  //on_boarding images
+  static const String deliveryDriverAvatar =
+      '$_imagePath/on_boarding/delivery_driver_avatar.svg';
+  static const String backgroundDeliveryDriverAvatar =
+      '$_imagePath/on_boarding/background_delivery_driver_avatar.svg';
+  static const String woman = '$_imagePath/on_boarding/woman_avatar.svg';
+  static const String man = '$_imagePath/on_boarding/man_avatar.svg';
+  static const String leftPhone = '$_imagePath/on_boarding/left_phone.svg';
+  static const String rightPhone = '$_imagePath/on_boarding/right_phone.svg';
+  static const String arrows = '$_imagePath/on_boarding/arrows.svg';
+  static const String flowers = '$_imagePath/on_boarding/flowers.svg';
+
+  //auth images
+  static const String createAccount = '$_imagePath/auth/create_account.svg';
+  static const String login = '$_imagePath/auth/login.svg';
+  static const String securityCode = '$_imagePath/auth/security_code.svg';
+
+  //add personal information images
+  static const String addPersonalInformation =
+      '$_imagePath/add_personal_information/add_personal_information.svg';
+
+  static const String addedPersonalInformationSuccessfully =
+      '$_imagePath/add_personal_information/added_successfully.svg';
+  static const String partyDecoration =
+      '$_imagePath/add_personal_information/party_decoration.svg';
+
+//////////////
   static const String addProduct = '$_imagePath/add_product.svg';
 
   //icons image
@@ -27,31 +54,8 @@ class ManagerAssets {
   static const String motorcycle = '$_imagePath/departments/motorcycle.svg';
   static const String phone = '$_imagePath/departments/phone.svg';
 
-  //login_and_create_account image
-  static const String successfullyState =
-      '$_imagePath/login_and_create_account/successfully_state.svg';
-  static const String addInformation =
-      '$_imagePath/login_and_create_account/add_information.svg';
-  static const String createAccount =
-      '$_imagePath/login_and_create_account/create_account.svg';
-  static const String partyDecoration =
-      '$_imagePath/login_and_create_account/party_decoration.svg';
-  static const String happyPerson =
-      '$_imagePath/login_and_create_account/happy_person.svg';
-  static const String login = '$_imagePath/login_and_create_account/login.svg';
-  static const String securityCode =
-      '$_imagePath/login_and_create_account/security_code.svg';
+  ///////////
 
-  //on_boarding image
-  static const String deliveryDriverAvatar =
-      '$_imagePath/on_boarding/delivery_driver_avatar.svg';
-  static const String backgroundDeliveryDriverAvatar =
-      '$_imagePath/on_boarding/background_delivery_driver_avatar.svg';
-  static const String woman = '$_imagePath/on_boarding/woman_avatar.svg';
-  static const String man = '$_imagePath/on_boarding/man_avatar.svg';
-  static const String leftPhone = '$_imagePath/on_boarding/left_phone.svg';
-  static const String rightPhone = '$_imagePath/on_boarding/right_phone.svg';
-  static const String arrows = '$_imagePath/on_boarding/arrows.svg';
-  static const String flowers = '$_imagePath/on_boarding/flowers.svg';
+  static const String happyPerson = '$_imagePath/auth/happy_person.svg';
   static const String addedSuccessfully = '$_imagePath/checking_boxes.svg';
 }
