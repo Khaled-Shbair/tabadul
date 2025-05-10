8b2fe7bd2da9d69eadbe6dc592ac3734dc7b644d
Khaled Shbair <khaled.shbair12@gmail.com>
2025-05-10 12:27:25 +0300

    remove userData key, because un-used

--- lib/config/constants/shared_preference_keys.dart
+++ lib/config/constants/shared_preference_keys.dart
@@ -1,11 +1,10 @@
 class SharedPreferenceKeys {
+  static const String viewOnBoarding = 'view_on_boarding';
   static const String loggedIn = 'logged_in';
   static const String phoneNumber = 'phone_number';
   static const String id = 'id';
-  static const String viewOnBoarding = 'view_on_boarding';
   static const String verificationId = 'verification_id';
   static const String name = 'name';
-  static const String userData = 'user_data';
   static const String city = 'city';
   static const String image = 'image';
   static const String regionAndStreet = 'region';
