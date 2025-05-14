e7359e65d4ba63088f9b7522889a2fec149bb668
Khaled Shbair <khaled.shbair12@gmail.com>
2025-05-14 17:40:43 +0300

    add new paths of images

--- lib/core/resource/manager_assets.dart
+++ lib/core/resource/manager_assets.dart
@@ -32,27 +32,35 @@ class ManagerAssets {
   static const String partyDecoration =
       '$_imagePath/add_personal_information/party_decoration.svg';
 
-//////////////
+  //home image
+  static const String bicycle = '$_imagePath/home/bicycle.svg';
+  static const String car = '$_imagePath/home/car.svg';
+  static const String carpet = '$_imagePath/home/carpet.svg';
+  static const String computer = '$_imagePath/home/computer.svg';
+  static const String electricMachines =
+      '$_imagePath/home/electric_machines.svg';
+  static const String furniture = '$_imagePath/home/furniture.svg';
+  static const String motorcycle = '$_imagePath/home/motorcycle.svg';
+  static const String phone = '$_imagePath/home/phone.svg';
+
+  //////////////
   static const String addProduct = '$_imagePath/add_product.svg';
 
   //icons image
   static const String homeIcon = '$_imagePath/icons_images/home_icon.svg';
+  static const String closeIcon = '$_imagePath/icons_images/close_icon.svg';
   static const String notificationIcon =
       '$_imagePath/icons_images/notification_icon.svg';
   static const String userIcon = '$_imagePath/icons_images/user_icon.svg';
+  static const String infoIcon = '$_imagePath/icons_images/info_icon.svg';
+
+  static const String logoutIcon = '$_imagePath/icons_images/logout_icon.svg';
+  static const String profileProvideServiceIcon =
+      '$_imagePath/icons_images/profile_provide_service_icon.svg';
+  static const String whatsAppIcon =
+      '$_imagePath/icons_images/whatsapp_icon.svg';
   static const String registerAsServiceProviderIcon =
       '$_imagePath/icons_images/register_as_service_provider_icon.svg';
-
-  //departments image
-  static const String bicycle = '$_imagePath/departments/bicycle.svg';
-  static const String car = '$_imagePath/departments/car.svg';
-  static const String carpet = '$_imagePath/departments/carpet.svg';
-  static const String computer = '$_imagePath/departments/computer.svg';
-  static const String electricMachines =
-      '$_imagePath/departments/electric_machines.svg';
-  static const String furniture = '$_imagePath/departments/furniture.svg';
-  static const String motorcycle = '$_imagePath/departments/motorcycle.svg';
-  static const String phone = '$_imagePath/departments/phone.svg';
 
   ///////////
 
