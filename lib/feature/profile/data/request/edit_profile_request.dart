909694ca72f1b653c2e202e31e83eb75e4943386
Khaled Shbair <khaled.shbair12@gmail.com>
2025-05-27 15:56:51 +0300

    create edit profile request

---
+++ lib/feature/profile/data/request/edit_profile_request.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import '/config/all_imports.dart';
+
+class EditProfileRequest {
+  String? firstName;
+  String? lastName;
+  File? uploadImage;
+  String? image;
+  String? region;
+  String? city;
+  String phoneNumber;
+
+  EditProfileRequest({
+    required this.phoneNumber,
+    this.firstName,
+    this.lastName,
+    this.uploadImage,
+    this.region,
+    this.city,
+  });
+
+  Map<String, dynamic> toMap() {
+    final data = <String, dynamic>{};
+
+    if (image != null) data[FirebaseConstants.image] = image;
+    if (firstName != null) data[FirebaseConstants.firstName] = firstName;
+    if (lastName != null) data[FirebaseConstants.lastName] = lastName;
+    if (city != null) data[FirebaseConstants.city] = city;
+    if (region != null) data[FirebaseConstants.regionAndStreet] = region;
+
+    return data;
+  }
+}
