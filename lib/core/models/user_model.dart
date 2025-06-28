554e523cb7c3dc0bb06248244d0f46fbf0b8d3d0
Khaled Shbair <khaled.shbair12@gmail.com>
2025-06-28 16:50:51 +0300

    Enhance UserModel by adding new fields for job, description, and imageOfProvideService. Updated constructor and toMap method to support these new attributes, improving data encapsulation and management for user profiles.

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -2,13 +2,31 @@ import '/config/all_imports.dart';
 
 class UserModel {
   String? id;
+
   String? firstName;
   String? lastName;
-  String? phoneNumber;
+
   String? city;
   String? regionAndStreet;
   String? image;
+  String? imageOfProvideService;
 
+  String? description;
+  String? job;
+  File? updateImage;
+  String? phoneNumber;
+
+  UserModel({
+    this.phoneNumber,
+    this.firstName,
+    this.lastName,
+    this.image,
+    this.regionAndStreet,
+    this.description,
+    this.job,
+    this.city,
+    this.updateImage,
+  });
   UserModel.formMap(Map<String, dynamic> map) {
     firstName = map[FirebaseConstants.firstName];
     lastName = map[FirebaseConstants.lastName];
@@ -17,5 +35,25 @@ class UserModel {
     city = map[FirebaseConstants.city];
     regionAndStreet = map[FirebaseConstants.regionAndStreet];
     image = map[FirebaseConstants.image];
+    description = map[FirebaseConstants.description];
+    job = map[FirebaseConstants.job];
+  }
+
+  Map<String, dynamic> toMap() {
+    final data = <String, dynamic>{};
+    if (image != null) data[FirebaseConstants.image] = image;
+    if (firstName != null) data[FirebaseConstants.firstName] = firstName;
+    if (lastName != null) data[FirebaseConstants.lastName] = lastName;
+    if (city != null) data[FirebaseConstants.city] = city;
+
+    if (imageOfProvideService != null) {
+      data[FirebaseConstants.imageOfProvideService] = imageOfProvideService;
+    }
+    if (description != null) data[FirebaseConstants.description] = description;
+    if (job != null) data[FirebaseConstants.job] = job;
+    if (regionAndStreet != null) {
+      data[FirebaseConstants.regionAndStreet] = regionAndStreet;
+    }
+    return data;
   }
 }
